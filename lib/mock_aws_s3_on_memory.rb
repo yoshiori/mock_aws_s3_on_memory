e01529f0fa65f9416f8973a407938ba4b30de309
Yoshiori SHOJI <yoshiori@gmail.com>
2014-05-23 18:33:26 +0900

    Add buckets

--- lib/mock_aws_s3_on_memory.rb
+++ lib/mock_aws_s3_on_memory.rb
@@ -1,5 +1,16 @@
 require "mock_aws_s3_on_memory/version"
+require "mock_aws_s3_on_memory/aws/s3/s3_object"
+require "mock_aws_s3_on_memory/aws/s3/paginated_collection"
+require "mock_aws_s3_on_memory/aws/s3/object_collection"
 
-module MockAwsS3OnMemory
-  # Your code goes here...
+class MockAwsS3OnMemory
+  class << self
+    def buckets
+      @buckets ||= Hash.new {|h, k| h[k] = {} }
+    end
+
+    def clear
+      buckets.clear
+    end
+  end
 end
--- spec/mock_aws_s3_on_memory_spec.rb
+++ spec/mock_aws_s3_on_memory_spec.rb
@@ -5,7 +5,25 @@ describe MockAwsS3OnMemory do
     expect(MockAwsS3OnMemory::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".buckets" do
+    it 'returns hash' do
+      expect(MockAwsS3OnMemory.buckets).to be_a Hash
+    end
+
+    it 'returns same instance' do
+      expect(MockAwsS3OnMemory.buckets).to be MockAwsS3OnMemory.buckets
+    end
+  end
+
+  describe ".clear" do
+    before do
+      MockAwsS3OnMemory.buckets[:hoge] = :foo
+    end
+
+    it 'empty buckets' do
+      expect {
+        MockAwsS3OnMemory.clear
+      }.to change { MockAwsS3OnMemory.buckets.size }.from(1).to(0)
+    end
   end
 end
